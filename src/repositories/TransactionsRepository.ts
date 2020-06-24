b6aab827850a9dec6337ab00011b5ef39031fd5a
Aleck Zanderbier <aleckzts@gmail.com>
2020-06-24 10:29:44 -0300

    testes ok

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,24 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance =  this.transactions.reduce((result, currentValue) => {
+      result[currentValue.type] += currentValue.value;
+      currentValue.type === 'income' ? result.total += currentValue.value : result.total -= currentValue.value;
+      return result;
+    }, { income: 0, outcome: 0, total: 0} as Balance);
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,27 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Invalid type, expected income or outcome');
+    }
+
+    if (isNaN(value)) {
+      throw Error('Value has to be a number');
+    }
+
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && balance.total - value < 0) {
+      throw Error('Cannot create outcome transaction without a valid balance')
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value: Number(value),
+      type,
+    });
+
+    return transaction;
   }
 }
 
